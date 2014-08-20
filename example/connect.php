74f3044baaad3ae24d046ca0becafb009034fd8e
yuduanchen <thisduan@gmail.com>
2014-08-20 12:01:24 +0800

    Update connect.php

--- example/connect.php
+++ example/connect.php
@@ -1,9 +1,7 @@
 <?php
 
 try {
-    $obj = new phdfs();
-    $obj->port = "9000";
-    $obj->ip = "127.0.0.1";
+    $obj = new phdfs("127.0.0.1","9000");
     $log = $obj->connect();
 } catch (Exception $ex) {
     var_dump($ex->getMessage());
