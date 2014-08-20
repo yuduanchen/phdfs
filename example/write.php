74dd40b73ef5a33c5494997f301a87b36f1a46b7
yuduanchen <thisduan@gmail.com>
2014-08-20 12:03:59 +0800

    Update write.php

--- example/write.php
+++ example/write.php
@@ -1,9 +1,7 @@
 <?php
 
 try {
-    $obj = new phdfs();
-    $obj->port = "9000";
-    $obj->ip = "127.0.0.1";
+    $obj = new phdfs("127.0.0.1","9000");
     $obj->connect();
     $log = $obj->write('/test.txt','hello world');
 } catch (Exception $ex) {
