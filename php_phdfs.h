764496c93c256bac157ae36fbcc8c209bd193c3e
yuduanchen <thisduan@gmail.com>
2014-09-09 09:17:37 +0800

    Update php_phdfs.h

--- php_phdfs.h
+++ php_phdfs.h
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Authors: yuduan chen <thisduan@gmail.com>                                  |
+  | Authors: yuduan chen <yumin1985@php.net>                             |
   +----------------------------------------------------------------------+
 */ 
 
